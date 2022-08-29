50e2c353bb7d7848f9846a58bc2362b0970d7c8d
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-08-29 15:15:05 -0500

    TEST: Get Region Methods
    It was added a restriction in order to only use a Guid type
    as an Id parameter in GetRegion method. It was added a condition
    to return a Not found when a region isn't found as well.

--- NZWalks/NZWalks.API/Controllers/RegionController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionController.cs
@@ -7,19 +7,19 @@ namespace NZWalks.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class RegionController : Controller
+    public class RegionsController : Controller
     {
         private readonly IRegionRepository regionRepository;
         private readonly IMapper mapper;
 
-        public RegionController(IRegionRepository regionRepository, IMapper mapper)
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
         {
             this.regionRepository = regionRepository;
             this.mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegionsAsync()
         {
             var regions = await regionRepository.GetAllAsync();
 
@@ -45,6 +45,21 @@ namespace NZWalks.API.Controllers
             var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
 
             return Ok(regionsDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")] /* https://localhost:port/{id} */
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+            return Ok(regionDTO);
         }
     }
 }
