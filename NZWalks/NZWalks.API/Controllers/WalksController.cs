63ba4012cfabcb0e9f25eb911441686111767ca4
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-09-01 13:29:10 -0500

    ADD: Get and Add Walk endpoint

--- NZWalks/NZWalks.API/Controllers/WalksController.cs
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
 using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
@@ -30,5 +31,47 @@ namespace NZWalks.API.Controllers
             return Ok(walksDTO);
         }
 
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkAsync")]
+        public async Task<IActionResult> GetWalkAsync(Guid id)
+        {
+            // Get walk domain object from database
+            var walkDomain = await  walkRepository.GetAsync(id);
+
+            // Convert Domain object to DTO
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walkDomain);
+
+            // Return response
+            return Ok(walkDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkAsync([FromBody] Models.DTO.AddWalkRequest addWalkRequest)
+        {
+            // Convert DTO to Domain Object
+            var walkDomain = new Models.Domain.Walk() { 
+                Length = addWalkRequest.Length,
+                Name = addWalkRequest.Name,
+                RegionId = addWalkRequest.RegionId,
+                WalkDifficultyId = addWalkRequest.WalkDifficultyId
+            };
+
+            // Pass domain object to Repository to persist this
+            walkDomain = await walkRepository.AddAsync(walkDomain);
+
+            // Convert the domain object back to DTO 
+            var walkDTO = new Models.DTO.Walk()
+            {
+                Id = walkDomain.Id,
+                Length = walkDomain.Length,
+                Name = walkDomain.Name,
+                RegionId = walkDomain.RegionId,
+                WalkDifficultyId = walkDomain.WalkDifficultyId
+            };
+
+            // Send DTO response back to client
+            return CreatedAtAction(nameof(GetWalkAsync), new { id = walkDTO.Id}, walkDTO);
+        }
     }
 }
