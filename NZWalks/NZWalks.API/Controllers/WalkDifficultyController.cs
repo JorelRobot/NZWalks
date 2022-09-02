59207642516d91fc9d819398008026ef17cecc45
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-09-02 15:03:05 -0500

    ADD: Get All Walk Difficulties endpoint

---
+++ NZWalks/NZWalks.API/Controllers/WalkDifficultyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalkDifficultyController : Controller
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+
+        public WalkDifficultyController(IWalkDifficultyRepository walkDifficultyRepository)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalkDifficultyAsycn()
+        {
+            return Ok(await walkDifficultyRepository.GetAllAsync());
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/WalkDifficultyRepository.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkDifficultyRepository : IWalkDifficultyRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+        private readonly IMapper mapper;
+
+        public WalkDifficultyRepository(NZWalksDbContext nZWalksDbContext, IMapper mapper)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+            this.mapper = mapper;
+        }
+
+        public Task<WalkDifficulty> AddAsync(WalkDifficulty walkDifficulty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WalkDifficulty> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<WalkDifficulty>> GetAllAsync()
+        {
+            return await nZWalksDbContext.WalkDifficulty.ToListAsync();
+        }
+
+        public Task<WalkDifficulty> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WalkDifficulty> UpdateAsycn(Guid id, WalkDifficulty walkDifficulty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
