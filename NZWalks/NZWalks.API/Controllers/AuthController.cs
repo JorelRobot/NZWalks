c3ba0f64e22ecd23ec19740652efcd43c08569fe
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-09-07 12:44:56 -0500

    ADD: Auth Controller

---
+++ NZWalks/NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IUserRepository userRespository;
+
+        public AuthController(IUserRepository userRespository)
+        {
+            this.userRespository = userRespository;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginAsync(Models.DTO.LoginRequest loginRequest)
+        {
+            // Validate the incoming request
+
+            // Check if user is authenticated
+
+            // Check username and password
+            var isAuthenticated = await userRespository.AuthenticateAsync(loginRequest.Username, loginRequest.Password);
+
+            if (isAuthenticated)
+            {
+                // Generate a JWT Token
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/StaticUserRepository.cs
@@ -0,0 +1,33 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class StaticUserRepository : IUserRepository
+    {
+        private List<User> Users = new List<User>()
+        {
+            new User()
+            {
+                FirstName = "Joel", LastName = "Rueda", EmailAddress = "jorel.robot@user.com",
+                Id = Guid.NewGuid(), Username = "JorelRobot", Password = "genericpass",
+                Roles = new List<string>() {"reader", "writer"}
+            },
+            new User()
+            {
+                FirstName = "Fernando", LastName = "Sustaita", EmailAddress = "fervnm155@user.com",
+                Id = Guid.NewGuid(), Username = "Fervnm155", Password = "genericpass",
+                Roles = new List<string>() {"reader"}
+            }
+        };
+
+        public async Task<bool> AuthenticateAsync(string username, string password)
+        {
+            var user = Users.Find(x => x.Username.Equals(username, StringComparison.InvariantCultureIgnoreCase) &&
+            x.Password == password);
+
+            if (user == null) return false;
+
+            return true;
+        }
+    }
+}
