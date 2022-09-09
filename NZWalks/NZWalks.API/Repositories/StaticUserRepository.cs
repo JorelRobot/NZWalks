de42da530010f16795c575b7b297b3593d36ebde
JorelRobot <joel.rueda.robles.sc@gmail.com>
2022-09-09 15:02:38 -0500

    REFACTOR: User and Roles in Database instead Static Users and Roles

--- NZWalks/NZWalks.API/Repositories/StaticUserRepository.cs
+++ NZWalks/NZWalks.API/Repositories/StaticUserRepository.cs
@@ -6,18 +6,18 @@ namespace NZWalks.API.Repositories
     {
         private List<User> Users = new List<User>()
         {
-            new User()
-            {
-                FirstName = "Joel", LastName = "Rueda", EmailAddress = "jorel.robot@user.com",
-                Id = Guid.NewGuid(), Username = "JorelRobot", Password = "genericpass",
-                Roles = new List<string>() {"reader", "writer"}
-            },
-            new User()
-            {
-                FirstName = "Fernando", LastName = "Sustaita", EmailAddress = "fervnm155@user.com",
-                Id = Guid.NewGuid(), Username = "Fervnm155", Password = "genericpass",
-                Roles = new List<string>() {"reader"}
-            }
+            //new User()
+            //{
+            //    FirstName = "Joel", LastName = "Rueda", EmailAddress = "jorel.robot@user.com",
+            //    Id = Guid.NewGuid(), Username = "JorelRobot", Password = "genericpass",
+            //    Roles = new List<string>() {"reader", "writer"}
+            //},
+            //new User()
+            //{
+            //    FirstName = "Fernando", LastName = "Sustaita", EmailAddress = "fervnm155@user.com",
+            //    Id = Guid.NewGuid(), Username = "Fervnm155", Password = "genericpass",
+            //    Roles = new List<string>() {"reader"}
+            //}
         };
 
         public async Task<User> AuthenticateAsync(string username, string password)
